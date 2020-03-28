f0ea452930161bcabfa8997101da1f996fa13392
MohamedHassan <mohamedhassannawar2020@gmail.com>
2020-03-28 14:41:29 +0200

    add Memo model

---
+++ MyMVVMRxSwiftExample/Model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  MyMVVMRxSwiftExample
+//
+//  Created by Mohamed Nawar on 3/28/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertData: Date
+    var identity: String
+    
+    init(content: String, insertData: Date, identity: String) {
+        self.content = content
+        self.insertData = insertData
+        self.identity = "\(insertData.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(orignal:Memo, updateContent: String) {
+        self = orignal
+        self.content = updateContent
+    }
+}
